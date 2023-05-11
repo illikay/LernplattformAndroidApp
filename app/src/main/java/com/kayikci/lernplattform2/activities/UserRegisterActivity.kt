e3c19cf1834abc9298413d9ad7de222699849eed
Ilkay Kayikci <ilkaykayikci@yahoo.de>
2023-05-11 15:01:44 +0200

    Validierung sollte jetzt vollständig sein.

--- app/src/main/java/com/kayikci/lernplattform2/activities/UserRegisterActivity.kt
+++ app/src/main/java/com/kayikci/lernplattform2/activities/UserRegisterActivity.kt
@@ -41,7 +41,7 @@ class UserRegisterActivity : AppCompatActivity() {
             val password = userRegisterBinding.passwordEditText.text.toString()
 
 
-            if (!validatePassword()) {
+            if (!validateUsername() || !validateEmail() || !validatePassword()) {
                 return@setOnClickListener
             }
 
@@ -83,21 +83,77 @@ class UserRegisterActivity : AppCompatActivity() {
     }
 
     private fun validatePassword(): Boolean {
-        val password: String = userRegisterBinding.passwordEditText.text.toString()
-        val checkPassword = Regex("^(?=.{10,}\$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\\W).*\$")
+        val password: String = userRegisterBinding.passwordEditText.text.toString().trim()
+        val checkPassword = Regex("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!?@#$%^&+=])(?=\\S+$).{8,}$")
+        //val checkSpaces = Regex("[^\\s]+")
 
         return if (password.isEmpty()) {
             userRegisterBinding.passwordEditText.error = "Passwort darf nicht leer sein"
             false
-        } else if (!password.matches(checkPassword)) {
+        }
+        else if (!password.matches(checkPassword)) {
             userRegisterBinding.passwordEditText.error = "Bitte ein Passwort, das mindestens 1 Großbuchstabe, 1 Kleinbuchstabe, 1 Ziffer, " +
-                    "1 Sonderzeichen enthält und eine Länge von mindestens 10 hat eingeben"
+                    "1 Sonderzeichen enthält, keine Leerzeichen enthält und eine Länge von mindestens 10 hat eingeben"
             false
-        } else {
+        }
+
+        else {
             userRegisterBinding.passwordEditText.error = null
             true
         }
     }
+
+    private fun validateUsername(): Boolean {
+        val firstName: String = userRegisterBinding.firstnameEditText.text.toString().trim()
+        val lastName: String = userRegisterBinding.lastnameEditText.text.toString().trim()
+        val checkSpaces = Regex("[^\\s]+")
+
+        return if (firstName.isEmpty()) {
+            userRegisterBinding.firstnameEditText.error = "Vorname darf nicht leer sein"
+            false
+        }
+        else if (lastName.isEmpty()) {
+            userRegisterBinding.lastnameEditText.error = "Nachname darf nicht leer sein"
+            false
+        }
+        else if (firstName.length > 20) {
+            userRegisterBinding.firstnameEditText.error = "Vorname ist zu lang"
+            false
+        }
+        else if (lastName.length > 20) {
+            userRegisterBinding.lastnameEditText.error = "Nachname ist zu lang"
+            false
+        }
+        else if (!firstName.matches(checkSpaces)) {
+            userRegisterBinding.firstnameEditText.error = "In Vorname sind keine Leerzeichen erlaubt"
+            false
+        }
+        else if (!lastName.matches(checkSpaces)) {
+            userRegisterBinding.lastnameEditText.error = "In Nachname sind keine Leerzeichen erlaubt"
+            false
+        }
+        else {
+            userRegisterBinding.firstnameEditText.error = null
+            userRegisterBinding.lastnameEditText.error = null
+            true
+        }
+    }
+
+    private fun validateEmail(): Boolean {
+        val email: String = userRegisterBinding.emailEditText.text.toString().trim()
+        val checkEmail = Regex("[a-zA-Z0-9._-]+@[a-z]+.+[a-z]+")
+        return if (email.isEmpty()) {
+            userRegisterBinding.emailEditText.error = "E-mail darf nicht leer sein"
+            false
+        } else if (!email.matches(checkEmail)) {
+            userRegisterBinding.emailEditText.error = "Das ist keine gültige E-mail"
+            false
+        } else {
+            userRegisterBinding.emailEditText.error = null
+            true
+        }
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
